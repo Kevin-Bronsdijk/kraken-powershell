bc2b56ff03d58c66b813db08bd5b62423750e987
Kevin Bronsdijk <bronsdijk@bronsdijk.com>
2016-08-29 20:05:59 +0700

    kraken-net update
    using the 1.2 by default

--- src/kraken-powershell/Properties/AssemblyInfo.cs
+++ src/kraken-powershell/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("kraken-powershell")]
 [assembly: AssemblyProductAttribute("kraken-powershell")]
 [assembly: AssemblyDescriptionAttribute("PowerShell module for Kraken.io")]
-[assembly: AssemblyVersionAttribute("0.1.0.5")]
-[assembly: AssemblyFileVersionAttribute("0.1.0.5")]
+[assembly: AssemblyVersionAttribute("0.1.0.6")]
+[assembly: AssemblyFileVersionAttribute("0.1.0.6")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "0.1.0.5";
-        internal const string InformationalVersion = "0.1.0.5";
+        internal const string Version = "0.1.0.6";
+        internal const string InformationalVersion = "0.1.0.6";
     }
 }
